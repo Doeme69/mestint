d0e10a6ca7c94f3690ec60bc89912ddbc403a383
Döme Balázs Márk <h044345@stud.u-szeged.hu>
2023-12-05 21:34:20 +0100

    gud

--- mestint/untitled/src/Agent.java
+++ mestint/untitled/src/Agent.java
@@ -27,19 +27,17 @@ public class Agent extends RaceTrackPlayer {
 
     List<Cell> visitedCells = new LinkedList<>();
     List<PathCell> coinpath = BFSBestCoin(state.i, state.j, track);
-    List<Direction> directions;
     ArrayList<Cell> pathSzakasz = new ArrayList<Cell>();
-    int tries;
     Direction currentDir;
+
+    boolean shouldSlowDown = false;
 
     @Override
     public Direction getDirection(long remainingTime) {
 
-
         updateVisitedCells();
 
-
-        if (iteration == 0){
+        if (iteration == 0) {
             sortCoinsByValue();
             pathSzakasz.add(new Cell(coinpath.get(0).i, coinpath.get(0).j));
             updatePathSzakasz(pathSzakasz.get(0));
@@ -47,25 +45,39 @@ public class Agent extends RaceTrackPlayer {
         }
         iteration++;
 
+        //ha vi-re van a vegtol akkor ugy lassitson hogy a vi 0 lesz
+        if ((state.i + state.vi == pathSzakasz.get(1).i && state.j + state.vj == pathSzakasz.get(1).j)) {
+            return new Direction(state.vi + state.vi * -2, state.vj + state.vj * -2);
+        }
+
+        //ha elerem a szakasz veget akkor update-elek
         if (state.i == pathSzakasz.get(1).i && state.j == pathSzakasz.get(1).j) {
             updatePathSzakasz(new Cell(state.i, state.j));
             getMaxVelocity();
+            return new Direction(state.vi * -1, state.vj * -1);
         }
 
-        if (state.vi == getMaxVelocity() || state.vj == getMaxVelocity()){
-            if ( state.vi != 1 && state.vj != 1){
-                slowDown();
-            }
+        //ha elerem azt a sebesseget amivel meg tudok meg allni idoben akkor lassulnia kell
+        if ((state.vi == getMaxVelocity().get(0) && state.vi != 0) || (state.vj == getMaxVelocity().get(1) && state.vj != 0)) {
+            shouldSlowDown = true;
+        }
+
+        //ha lassulnia kell akkor amig a velocityje nem 1 addig lassuljon
+        if (shouldSlowDown) {
+            if ((state.vi == 1 || state.vi == -1) || (state.vj == 1 || state.vj == -1)) {
+                shouldSlowDown = false;
+            } else
+                return slowDown();
         }
 
         return currentDir;
-    }
 
+    }
 
     //ELindul, belerakom az elso elemet a listaba, kivonom a lista kovi elemebol a kezdopontot, az lesz a direction, majd a coinpath kovetkezo elemebol vonom ki a jelenlegi vegpontot
     //ha ez a kulonbseg megegyezik a jelenlegi directionnel akkor ez lesz az uj vegpont,
     //ha pedig ezek kulonbsege mas mint a direction akkor ez lesz a szakasz majd a vegpont lesz a kezdopontja a kovetkezo szakasznak
-    public List<Cell> updatePathSzakasz(Cell kezdopont) {
+    public void updatePathSzakasz(Cell kezdopont) {
         Cell vegpont;
 
         if (pathSzakasz.size() < 2) {
@@ -74,30 +86,39 @@ public class Agent extends RaceTrackPlayer {
             pathSzakasz.set(0, kezdopont);
         }
         vegpont = coinpath.get(coinpath.indexOf(kezdopont) + 1);
-        currentDir = new Direction (vegpont.i - pathSzakasz.get(1).i, vegpont.j - pathSzakasz.get(1).j);
+        currentDir = new Direction(vegpont.i - pathSzakasz.get(1).i, vegpont.j - pathSzakasz.get(1).j);
 
         for (int i = 1; i < coinpath.size(); i++) {
             vegpont = coinpath.get(coinpath.indexOf(kezdopont) + i);
 
-            if (currentDir.i == vegpont.i - pathSzakasz.get(1).i && currentDir.j == vegpont.j - pathSzakasz.get(1).j){
+            if (currentDir.i == vegpont.i - pathSzakasz.get(1).i && currentDir.j == vegpont.j - pathSzakasz.get(1).j) {
                 pathSzakasz.set(1, vegpont);
-            }
-            else break;
+            } else
+                break;
         }
 
-        return pathSzakasz;
+        System.out.println(pathSzakasz);
     }
 
-    public Direction slowDown(){
+    //ugy lassit hogy az irany ellentetet adja vissza
+    public Direction slowDown() {
         return new Direction(currentDir.i * -1, currentDir.j * -1);
     }
 
+    //kb csak a manhattan tavolsaga a szakasz elejenek es vegenek
     public Cell getDistance() {
         return new Cell(Math.abs(pathSzakasz.get(1).i - pathSzakasz.get(0).i), Math.abs(pathSzakasz.get(1).j - pathSzakasz.get(0).j));
     }
 
-    public int getMaxVelocity(){
-        return Math.max((int) Math.sqrt(getDistance().i), (int) Math.sqrt(getDistance().j));
+    public List<Integer> getMaxVelocity() {
+        List<Integer> MaxVelocity = new ArrayList<>();
+
+        int maxVelocityX = (int)Math.sqrt(getDistance().i);
+        int maxVelocityY = (int)Math.sqrt(getDistance().j);
+        MaxVelocity.add(maxVelocityX);
+        MaxVelocity.add(maxVelocityY);
+
+        return MaxVelocity;
     }
 
     public List<PathCell> BFSBestCoin(int i, int j, int[][] track) {
